030718bd833ec3d39b867a65ef17dfe74c46da4e
Pamela Garcia <pamela.garcia01@epn.edu.ec>
2026-05-21 00:25:50 -0500

    Actualizacion clases Producto, CatalogoProducto y DetalleProducto

--- SistemaFacturacionPapelería/src/stock/DetalleProducto.java
+++ SistemaFacturacionPapelería/src/stock/DetalleProducto.java
@@ -5,6 +5,23 @@ public class DetalleProducto {
     protected int cantidad;
     protected double subtotal;
 
+public DetalleProducto() {
+
+}
+
+    public int getCantidad() { 
+        return cantidad; 
+        }
+    public void setCantidad(int cantidad) { 
+        this.cantidad = cantidad; 
+        }
+    public double getSubtotal() { 
+        return subtotal; 
+        }
+    public void setSubtotal(double subtotal) { 
+        this.subtotal = subtotal; 
+        }
+
     public double calcularSubtotal(double precio) {
         return 0.0;
     }
